e0c8ba6762f537631335119aaa2a51331af38e19
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-15 12:53:11 +0530

    create interface

---
+++ src/app/crib.ts
@@ -0,0 +1,11 @@
+export interface Crib {
+  id: number,
+  type: string,
+  price: number,
+  adress: string,
+  description: string,
+  bedrooms: number,
+  bathroom: number,
+  area: number,
+  image: string,
+}
